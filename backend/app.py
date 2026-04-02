a8a750fc76e4e0abb44b24429e6bcceeea07126b
RohitMunjal25 <mobilemunjal@gmail.com>
2026-04-03 00:50:23 +0530

    cors problem

--- backend/app.py
+++ backend/app.py
@@ -21,8 +21,14 @@ from models.audiospoof import detect_audio_spoof
 from backendstore import UPLOAD_FOLDER
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 bcrypt = Bcrypt(app)
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+    return response
 
 load_dotenv()
 ensure_models_exist()
