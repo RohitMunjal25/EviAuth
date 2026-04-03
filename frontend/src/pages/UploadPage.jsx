9d2373b57e57d9153ef71fcafdae87119265e228
RohitMunjal25 <mobilemunjal@gmail.com>
2026-04-03 13:51:43 +0530

    cors

--- frontend/src/pages/UploadPage.jsx
+++ frontend/src/pages/UploadPage.jsx
@@ -27,7 +27,7 @@ export default function UploadPage({ goScan }) {
     formData.append("file", file);
 
     try {
-      const response = await fetch("https://sheffield-entrance-fashion-tim.trycloudflare.com/upload", {
+      const response = await fetch("https://dvd-quiz-participation-appreciate.trycloudflare.com/upload", {
         method: "POST",
         body: formData,
       });
